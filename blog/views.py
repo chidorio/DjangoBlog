d21ff9610da3ef76e45743ab828a35b12ce186d1
GG <mymrin.gm@gmail.com>
2023-08-20 11:44:13 +0300

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Corey',
+        'title': 'Blog post1',
+        'content': 'Some content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'gg',
+        'title': 'Blog post2',
+        'content': 'Some content2',
+        'date_posted': 'August 26, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
