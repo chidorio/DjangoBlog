e4ecca7c93545161269f4fd1427d5f9b2d9b558f
GG <mymrin.gm@gmail.com>
2023-08-22 09:00:43 +0300

    add DB

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Corey',
-        'title': 'Blog post1',
-        'content': 'Some content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'gg',
-        'title': 'Blog post2',
-        'content': 'Some content2',
-        'date_posted': 'August 26, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
